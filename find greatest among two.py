58d809852fdf29537b4c7f71530692584b275846
abhisheknarendar <abhisheknarendar27@gmail.com>
2025-01-26 22:33:14 +0530

    Add files via upload

---
+++ find greatest among two.py
@@ -0,0 +1,8 @@
+#Find geatest among 2 mystery numbers
+n1 = int(input("Enter mystery number 1:"))
+n2 = int(input("Enter mystery number 2:"))
+
+if n1>n2:
+    print("The greatest number is", n1)
+else:
+    print("The greatest number is", n2)
---
+++ find relations between two myster numbers.py
@@ -0,0 +1,10 @@
+#Find relations between 2 mystery numbers
+n1 = int(input("Enter mystery number 1:"))
+n2 = int(input("Enter mystery number 2:"))
+
+print(f"{n1}<{n2} is {n1<n2}")
+print(f"{n1}<={n2} is {n1<=n2}")
+print(f"{n1}>{n2} is {n1>n2}")
+print(f"{n1}>={n2} is {n1>=n2}")
+print(f"{n1}=={n2} is {n1==n2}")
+print(f"{n1}!={n2} is {n1!=n2}")
